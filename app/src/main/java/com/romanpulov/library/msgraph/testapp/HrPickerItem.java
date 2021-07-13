5ac37f3daee449161256221b4eb1361a4553e310
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-07-13 17:59:25 +0300

    eod

--- app/src/main/java/com/romanpulov/library/msgraph/testapp/HrPickerItem.java
+++ app/src/main/java/com/romanpulov/library/msgraph/testapp/HrPickerItem.java
@@ -4,6 +4,7 @@ public class HrPickerItem {
     public static int ITEM_TYPE_FOLDER = 1;
     public static int ITEM_TYPE_FILE = 2;
     public static int ITEM_TYPE_PARENT = 3;
+    public static int ITEM_TYPE_UNKNOWN = 4;
 
     public final int itemType;
     public final String name;
@@ -11,5 +12,17 @@ public class HrPickerItem {
     public HrPickerItem(int itemType, String name) {
         this.itemType = itemType;
         this.name = name;
+    }
+
+    public static HrPickerItem createParentItem() {
+        return new HrPickerItem(ITEM_TYPE_PARENT, null);
+    }
+
+    public static HrPickerItem createItem(int itemType, String name) {
+        if (itemType == ITEM_TYPE_PARENT) {
+            throw new RuntimeException("Error creating parent item, use a separate method for that");
+        } else {
+            return new HrPickerItem(itemType, name);
+        }
     }
 }
--- app/src/main/java/com/romanpulov/library/msgraph/testapp/MSGraphHelper.java
+++ app/src/main/java/com/romanpulov/library/msgraph/testapp/MSGraphHelper.java
@@ -7,8 +7,12 @@ import com.romanpulov.library.msgraph.MSALConfig;
 import com.romanpulov.library.msgraph.MSGraphBaseHelper;
 import com.romanpulov.library.msgraph.OnMSActionListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MSGraphHelper extends MSGraphBaseHelper implements HrPickerNavigator {
@@ -36,6 +40,29 @@ public class MSGraphHelper extends MSGraphBaseHelper implements HrPickerNavigato
 
     }
 
+    private static List<HrPickerItem> parseJSONObject(JSONObject data) throws JSONException {
+        List<HrPickerItem> result = new ArrayList<>();
+
+        JSONArray jsonItems = data.getJSONArray("value");
+        for (int i = 0; i < jsonItems.length(); i ++) {
+            JSONObject o = (JSONObject) jsonItems.get(i);
+
+            int itemType;
+
+            if (!o.isNull("folder")) {
+                itemType = HrPickerItem.ITEM_TYPE_FOLDER;
+            } else if (!o.isNull("file")) {
+                itemType = HrPickerItem.ITEM_TYPE_FILE;
+            } else {
+                itemType = HrPickerItem.ITEM_TYPE_UNKNOWN;
+            }
+
+            result.add(HrPickerItem.createItem(itemType, o.getString("name")));
+        }
+
+        return result;
+    }
+
     @Override
     synchronized public void onNavigate(Context context, String path, HrPickerItem item, HrPickerNavigationProcessor processor) {
         if (!mNavigating.get()) {
@@ -51,6 +78,11 @@ public class MSGraphHelper extends MSGraphBaseHelper implements HrPickerNavigato
                         @Override
                         public void onActionSuccess(int action, JSONObject data) {
                             Log.d(TAG, "Obtained data:" + data.toString());
+                            try {
+                                processor.onNavigationSuccess(path, parseJSONObject(data));
+                            } catch (JSONException e) {
+                                processor.onNavigationFailure(path, "Error parsing response: " + e.getMessage());
+                            }
                         }
 
                         @Override
